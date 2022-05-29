223769050bd89fbd15f41b864d7e01a1276a1240
JRARM <77474292+JRARM@users.noreply.github.com>
2022-05-29 13:33:25 -0500

    Updated Things

--- Navigation.js
+++ Navigation.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
 //screens
 import ShopScreen from "./screens/ShopScreen";
 import ShoppingCartScreen from "./screens/ShoppingCartScreen";
@@ -10,25 +9,27 @@ import AccountScreen from "./screens/AccountScreen";
 
 //icons
 
-
-import { AntDesign } from '@expo/vector-icons'; 
-import { Entypo } from '@expo/vector-icons'; 
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { AntDesign } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
     return (
         <Tab.Navigator
-            initialRouteName='Shop' screenOptions={{ 
-                tabBarActiveTintColor: 'orange', }}>
+
+            initialRouteName='Shop'
+            screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: 'orange',
+
+            }}
+
+        >
             <Tab.Screen
-                name="Shop"
-                component={ShopScreen}
-                options={{
-                    tabBarLabel: 'Tienda',
-                    tabBarIcon: ({ color, size }) => {
-                        <Ionicons name="md-checkmark-circle" size={size} color={color} />
-                    }
+                name="Shop"component={ShopScreen} options={{
+                    
                 }}
             />
             <Tab.Screen name="ShoppingCart" component={ShoppingCartScreen} />
--- screens/ShopScreen.js
+++ screens/ShopScreen.js
@@ -1,17 +1,83 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { FlatList } from 'react-native-gesture-handler';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item3',
+  }, {
+    id: '58694a0f-3da12-471f-bd96-145571e29d72',
+    title: 'Third Item4',
+  }, {
+    id: '58694a0f-3da13-471f-bd96-145571e29d72',
+    title: 'Third Item5',
+  }, {
+    id: '58694a0f-3da14-471f-bd96-145571e29d72',
+    title: 'Third Item6',
+  }, {
+    id: '58694a0f-3da15-471f-bd96-145571e29d72',
+    title: 'Third Item7',
+  }, {
+    id: '58694a0f-3da16-471f-bd96-145571e29d72',
+    title: 'Third Item8',
+  }, {
+    id: '58694a0f-3da17-471f-bd96-145571e29d72',
+    title: 'Third Item9',
+  }, {
+    id: '58694a0f-3da18-471f-bd96-145571e29d72',
+    title: 'Third Item10',
+  }
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+
 const ShopScreen = () => {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
   return (
-    <View>
-      <Text>ShopScreen</Text>
-      <Ionicons name="md-checkmark-circle" size={200} color="purple" />
-      <Ionicons name="md-checkmark-circle" size={300} color="green" />
-      <Ionicons name="md-checkmark-circle" size={400} color="blue" />
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+
+  );
 }
 
 export default ShopScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
